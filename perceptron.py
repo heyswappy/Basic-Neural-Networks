2234fccabd8cf318ba4a0694480f3b171f7d903c
Swapnil Saxena <30383437+heyswappy@users.noreply.github.com>
2019-09-19 12:37:30 +0530

    Update and rename 2.perceptron.py to perceptron.py

---
+++ perceptron.py
@@ -0,0 +1,58 @@
+class Perceptron:
+	def __init__(self, n, rate=1, thresh=1):
+		self.count = n+1
+		self.rate = rate
+		self.thresh = thresh
+		self.edges = [0]*(n+1)
+	
+	def activation(self, x):
+		if x > self.thresh:
+			return 1
+		elif x < -1*self.thresh:
+			return -1
+		else:
+			return 0
+	
+	def _update(self, s, z):
+		# s --> input
+		# z --> output
+		self.edges[0] += 1*z*self.rate
+		for i in range(1, self.count):
+			self.edges[i] += s[i-1]*z*self.rate
+		return
+
+	def fit(self, s, z, info=False):
+		# s -->  list of inputs
+		# z --> list of corresponding outputs
+		# info --> show information or not
+		flag = True
+		while(flag):
+			flag = False
+			for i in range(len(s)):
+				if self.predict(s[i]) != z[i]:
+					flag = True
+					self._update(s[i],z[i])
+		if not info :
+			return
+		print("Training Complete")
+		for i in range(len(s)):
+			out = self.predict(s[i])
+			print("Output is: ",out," should be: ",z[i])
+		return
+
+	def predict(self, s):
+		# s is an input
+		ans = self.edges[0]
+		for i in range(1,self.count):
+			ans += s[i-1]*self.edges[i]
+		return self.activation(ans)
+
+if __name__ == "__main__":
+	n = int(input("The number of neurons:"))
+	t = int(input("The number of inputs(training_data_count):"))
+	s = [list(map(int,input("Input? :").strip(" ").split(" "))) for i in range(t)]
+	z = [int(input("Output? :")) for i in range(t)]
+	net = Perceptron(n)
+	net.fit(s, z, False)
+	for i in s:
+		print(net.predict(i))
